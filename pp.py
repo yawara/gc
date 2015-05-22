d629bb022be3f24807d159e3a2b1e3fd0ab8e0a7
yawara <ni732h.sphl@gmail.com>
2015-05-23 03:02:55 +0900

    a

--- pp.py
+++ pp.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python3
 import networkx as nx
 from matplotlib import pyplot as plt
+from erdos import rtv
 
-p = 13
+def get(p):
+  lines = set()
+  for i in range(p):
+    for j in range(p):
+      for k in range(p):
+        if all(not ((a*i)%p,(a*j)%p,(a*k)%p) in lines for a in range(1,p)):
+          lines.add((i,j,k)) 
+  
+  lines.remove((0,0,0))
+  #print(lines)
 
-lines = set()
-for i in range(p):
-  for j in range(p):
-    for k in range(p):
-      if all(not ((a*i)%p,(a*j)%p,(a*k)%p) in lines for a in range(1,p)):
-        lines.add((i,j,k)) 
+  G = nx.Graph()
 
-lines.remove((0,0,0))
-print(lines)
+  for line1 in lines:
+    for line2 in lines:
+      if line1 != line2:
+        norm = 0
+        for i in range(3):
+          norm += line1[i]*line2[i]
+        if norm % p == 0:
+          G.add_edge(line1,line2)
 
-G = nx.Graph()
+  return G
 
-for line1 in lines:
-  for line2 in lines:
-    if line1 != line2:
-      norm = 0
-      for i in range(3):
-        norm += line1[i]*line2[i]
-      if norm % p == 0:
-        G.add_edge(line1,line2)
-
-
+def show(p):
+  G = get(p)
+  print("diameter:", nx.diameter(G))
+  print("node:", len(G.nodes()))
+  if len(G.nodes()) in rtv:
+    print("FAIL")
+  print(G.degree().values())
