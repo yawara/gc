611a018e2ec9ca2478a10cf05f8ea70bc56317f7
yawara <ni732h.sphl@gmail.com>
2015-06-24 15:53:07 +0900

    m

---
+++ search_poly.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import pp
+import pyprimes
+
+def order(p,n,m):
+  N = p**(n*m)
+  M = p**(n*(m-1))
+  return N**2 + N*M + M**2
+
+def degree(p,n,m):
+  N = p**(n*m)
+  M = p**(n*(m-1))
+  return N + M
+
+def pair(p,n,m):
+  return order(p,n,m),degree(p,n,m)
+
+def search(max_p=1000,max_k=100,max_n=10,max_m=10):
+  qs = []
+  for p in pyprimes.primes_below(max_p):
+    for k in range(1,max_k):
+      qs.append(p**k)
+  qs.sort()
+  print("constructed qs", len(qs))
+  
+  i = 0
+  for q in qs:
+    print(i)
+    for m in range(2,max_m):
+      for n in range(1,max_n):
+        print(n,m)
+        tmp = pair(q,n,m)
+        if pp.is_ddp_new(*tmp):
+          print(tmp)
+          print(p,k,n,m)
+          print("")
+    i+=1
