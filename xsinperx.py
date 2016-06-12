15d8d80d93023fdaa8a654d8795763cc3841378f
Yawara ISHIDA <yawara@kurims.kyoto-u.ac.jp>
2016-06-13 08:51:00 +0900

    almost finished

--- xsinperx.py
+++ xsinperx.py
@@ -4,10 +4,15 @@ import matplotlib.pyplot as plt
 
 X = np.arange(-1, 1, 0.003)
 Y = X * np.sin(1 / X)
-
 plt.plot(X, Y)
-plt.plot(X, X)
-plt.plot(X, -X)
+plt.plot(X, X, '--k')
+plt.plot(X, -X, '--k')
+
+X = np.asarray([2 / (i * np.pi) for i in range(1, 10)] + [-2 / (i * np.pi) for i in range(1, 10)])
+Y = X * np.sin(1 / X)
+plt.annotate('test', (0, 0))
+plt.plot(X, Y, 'ro')
+
 plt.show()
 
 #sin(1 / x)
